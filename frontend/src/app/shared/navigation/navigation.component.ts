5ba14eea54e173ed5cdc48ac57be9ecfd671af9a
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2020-05-13 14:42:03 -0400

    Navigation style

--- frontend/src/app/shared/navigation/navigation.component.ts
+++ frontend/src/app/shared/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/providers/services/auth.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
 }
